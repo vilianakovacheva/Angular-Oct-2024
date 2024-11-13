fdd4cf6311437f07f239aaccf7f3b063bcee3a70
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-11-13 12:48:38 +0200

    first workshop for the softuni forum project

---
+++ Workshop/workshop/src/app/main/themes-list/themes-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Theme } from '../../types/theme';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+
+@Component({
+  selector: 'app-themes-list',
+  standalone: true,
+  imports: [LoaderComponent],
+  templateUrl: './themes-list.component.html',
+  styleUrl: './themes-list.component.css'
+})
+export class ThemesListComponent implements OnInit {
+  themes: Theme[] = [];
+  isLoading = true;
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(){
+    this.apiService.getThemes().subscribe((themes) => {
+      this.themes = themes;
+      this.isLoading = false;
+    })
+  }
+}
